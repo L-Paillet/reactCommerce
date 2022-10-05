dc154191415d285c20d2e725409a30a98d0c96df
laura <l.paillet@ecole-ipssi.net>
2022-10-05 16:56:21 +0200

    footer element

--- site-e-commerce/src/component/Footer.js
+++ site-e-commerce/src/component/Footer.js
@@ -1,17 +1,47 @@
 import "../css/Footer.css";
+import { Link } from "react-router-dom";
+import WWE from "../asset/WWE.png"
+
+
 function Footer() {
     const numero = "06.12.34.56.78";
     const site = "https://ecole-ipssi.com";
     return (
         <div className="footer">
-            <h4>
-                Compléments d'informations
-            </h4>
-            <ul className="liste">
-                <li> numéro : {numero}</li>
-                <li> site officiel: <a href={site}>IPSSI</a></li>
-                <li> all right reserved</li>
-            </ul>
+            <img src={WWE} className="logoFooter" alt="logo Ipssi"/>
+            <div className="containerCard">
+                <div className="cardFooter">
+                    <h4>
+                        Compléments d'informations
+                    </h4>
+                    <ul className="liste">
+                        <li> numéro : {numero}</li>
+                        <li> site officiel: <a href={site}>IPSSI</a></li>
+                    </ul>
+                </div>
+                <div className="cardFooter">
+                    <h4>
+                        Notre équipe
+                    </h4>
+                    <p className="liste">
+                        <li><Link to="/notre-equipe">A propos de nous</Link></li>
+                        <li> site officiel: <a href={site}>IPSSI</a></li>
+                    </p>
+                </div>
+                <div className="cardFooter">
+                    <h4>
+                        Nous contactez
+                    </h4>
+                    <ul className="liste">
+                        <p><Link className="linkFooter" to="/contact">Contact</Link></p>
+                        <li> site officiel: <a href={site}>IPSSI</a></li>
+                    </ul>
+                </div>
+            </div>
+            <span>
+                <p>Vous devez être âgé(e) d’au moins 18 ans pour faire des achats en ligne.Tous droits réservés. 
+                    L'utilisation de ce site implique que vous acceptez les conditions d'utilisation.</p>
+            </span>
         </div>
     )
 }
